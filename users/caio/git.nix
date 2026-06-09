a8aa70a7850ab2c5677e76e24f0efe6aa70677f2
caionoliver <caionoliver.contato@gmail.com>
2026-06-09 19:05:23 -0300

    feat(git): added allowed_signers file

--- users/caio/git.nix
+++ users/caio/git.nix
@@ -1,4 +1,4 @@
-{ config, inputs, pkgs, ... }:
+{ config, inputs, pkgs, homeDir, ... }:
 
 {
   programs.git = {
@@ -6,9 +6,10 @@
     settings = {
       user.name = "caionoliver";
       user.email = "caionoliver.contato@gmail.com";
-      user.signingkey = "~/.ssh/id_ed25519.pub";
+      user.signingkey = "${homeDir}/.ssh/id_ed25519.pub";
       commit.gpgsign = true;
       gpg.format = "ssh";
+      gpg.ssh.allowedSignersFile = "${homeDir}/.ssh/allowed_signers";
     };
 
     ignores = [
